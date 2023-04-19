fb256ce399c15ea373e9ef91ba1e4a08b499e9fa
webtactics <paul@webtactics.com.au>
2023-04-19 13:26:57 +1000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -114,6 +114,10 @@ module.exports = function(eleventyConfig) {
       includes: "_includes",
       data: "_data",
       output: "_site"
-    }
+    },
+    templateFormats: [ "md", "njk", "html", ],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
   };
 };
